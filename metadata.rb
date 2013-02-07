e2d8cd6fe425c388718291da75c8c8afc60fef3e
Scott M. Likens <scott@likens.us>
2013-02-06 19:19:10 -0800

    bump to 0.0.6

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "scott@likens.us"
 license          "Apache 2.0"
 description      "Installs/Configures carbon"
 long_description description
-version          "0.0.5"
+version          "0.0.6"
 %w{build-essential python runit git}.each do |dp|
   depends dp
 end
