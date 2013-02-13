bd7686119a69c2aaad81e6cbb577d95925947159
Scott M. Likens <scott@likens.us>
2013-02-12 18:33:31 -0800

    bump to 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "scott@likens.us"
 license          "Apache 2.0"
 description      "Installs/Configures carbon"
 long_description description
-version          "0.0.6"
+version          "0.1.0"
 supports 'ubuntu', "= 12.04"
 depends 'runit', "= 1.0.3"
 depends 'build-essential'
