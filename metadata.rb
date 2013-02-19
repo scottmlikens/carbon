c9bfc88c812d8fe3a5b8ddf575606b97d57ed8a5
Scott M. Likens <scott@likens.us>
2013-02-18 20:47:32 -0800

    loosen the versioning

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      "Installs/Configures carbon"
 long_description description
 version          "0.1.0"
 supports 'ubuntu', "= 12.04"
-depends 'runit', "= 1.0.3"
+depends 'runit'
 depends 'build-essential'
 depends 'python'
 depends 'git'
