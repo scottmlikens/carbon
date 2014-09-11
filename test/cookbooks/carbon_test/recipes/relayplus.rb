f0d928f5ff4e29c19db2be10b6d16d50cdb8b4fb
Scott M. Likens <scott@likens.us>
2014-09-11 15:44:26 -0700

    remove cpu affinity from tests and recipes in tests

--- test/cookbooks/carbon_test/recipes/relayplus.rb
+++ test/cookbooks/carbon_test/recipes/relayplus.rb
@@ -4,13 +4,11 @@ end
 
 carbon_cache "carbon-cache-a" do
   action :create
-  cpu_affinity 1
   init_style "runit"
   storage_aggregation({:min => { :pattern => "\.min$", :xfilesfactor => "0.1", :aggregationmethod => "min" }, :max => { :pattern => "\.max$", :xfilesfactor => "0.1", :aggregationmethod => "max" }, :sum => { :pattern => "\.count$", :xfilesfactor => "0", :aggregationmethod => "sum" }, :default_average => { :pattern => ".*", :xfilesfactor => "0.5", :aggregationmethod => "average"}})
 end
 
 carbon_relay "carbon-relay-a" do
-  cpu_affinity 0
   init_style "runit"
   action :create
 end
